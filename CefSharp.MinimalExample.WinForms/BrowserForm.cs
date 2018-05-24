7836552dbd983f4360e460d38452fa108f8868e1
Jarosław Rejterada <j.rejterada@hakon.pl>
2018-05-24 12:39:32 +0200

    show new window with random printing from template dictionary

--- CefSharp.MinimalExample.WinForms/BrowserForm.cs
+++ CefSharp.MinimalExample.WinForms/BrowserForm.cs
@@ -1,17 +1,19 @@
 ﻿// Copyright © 2010-2015 The CefSharp Authors. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-
 using System;
+using System.IO;
 using System.Windows.Forms;
-using CefSharp.MinimalExample.WinForms.Controls;
 using CefSharp.WinForms;
 
 namespace CefSharp.MinimalExample.WinForms
 {
+    using System.Collections.Generic;
+
     public partial class BrowserForm : Form
     {
-        private readonly ChromiumWebBrowser browser;
+        private List<string> _printings = new List<string>();
+        Random random = new Random();
 
         public BrowserForm()
         {
@@ -20,139 +22,44 @@ namespace CefSharp.MinimalExample.WinForms
             Text = "CefSharp";
             WindowState = FormWindowState.Maximized;
 
-            browser = new ChromiumWebBrowser("www.google.com")
+            var files = Directory.EnumerateFiles("Templates", "*.html");
+            foreach (var file in files)
             {
-                Dock = DockStyle.Fill,
-            };
-            toolStripContainer.ContentPanel.Controls.Add(browser);
-
-            browser.LoadingStateChanged += OnLoadingStateChanged;
-            browser.ConsoleMessage += OnBrowserConsoleMessage;
-            browser.StatusMessage += OnBrowserStatusMessage;
-            browser.TitleChanged += OnBrowserTitleChanged;
-            browser.AddressChanged += OnBrowserAddressChanged;
-
-            var bitness = Environment.Is64BitProcess ? "x64" : "x86";
-            var version = String.Format("Chromium: {0}, CEF: {1}, CefSharp: {2}, Environment: {3}", Cef.ChromiumVersion, Cef.CefVersion, Cef.CefSharpVersion, bitness);
-            DisplayOutput(version);
-        }
-
-        private void OnBrowserConsoleMessage(object sender, ConsoleMessageEventArgs args)
-        {
-            DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", args.Line, args.Source, args.Message));
-        }
-
-        private void OnBrowserStatusMessage(object sender, StatusMessageEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => statusLabel.Text = args.Value);
-        }
-
-        private void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
-        {
-            SetCanGoBack(args.CanGoBack);
-            SetCanGoForward(args.CanGoForward);
-
-            this.InvokeOnUiThreadIfRequired(() => SetIsLoading(!args.CanReload));
-        }
-
-        private void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => Text = args.Title);
-        }
-
-        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => urlTextBox.Text = args.Address);
-        }
-
-        private void SetCanGoBack(bool canGoBack)
-        {
-            this.InvokeOnUiThreadIfRequired(() => backButton.Enabled = canGoBack);
-        }
-
-        private void SetCanGoForward(bool canGoForward)
-        {
-            this.InvokeOnUiThreadIfRequired(() => forwardButton.Enabled = canGoForward);
-        }
-
-        private void SetIsLoading(bool isLoading)
-        {
-            goButton.Text = isLoading ?
-                "Stop" :
-                "Go";
-            goButton.Image = isLoading ?
-                Properties.Resources.nav_plain_red :
-                Properties.Resources.nav_plain_green;
-
-            HandleToolStripLayout();
-        }
-
-        public void DisplayOutput(string output)
-        {
-            this.InvokeOnUiThreadIfRequired(() => outputLabel.Text = output);
-        }
-
-        private void HandleToolStripLayout(object sender, LayoutEventArgs e)
-        {
-            HandleToolStripLayout();
-        }
-
-        private void HandleToolStripLayout()
-        {
-            var width = toolStrip1.Width;
-            foreach (ToolStripItem item in toolStrip1.Items)
-            {
-                if (item != urlTextBox)
-                {
-                    width -= item.Width - item.Margin.Horizontal;
-                }
-            }
-            urlTextBox.Width = Math.Max(0, width - urlTextBox.Margin.Horizontal - 18);
-        }
-
-        private void ExitMenuItemClick(object sender, EventArgs e)
-        {
-            browser.Dispose();
-            Cef.Shutdown();
-            Close();
-        }
-
-        private void GoButtonClick(object sender, EventArgs e)
-        {
-            LoadUrl(urlTextBox.Text);
-        }
-
-        private void BackButtonClick(object sender, EventArgs e)
-        {
-            browser.Back();
-        }
-
-        private void ForwardButtonClick(object sender, EventArgs e)
-        {
-            browser.Forward();
-        }
-
-        private void UrlTextBoxKeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode != Keys.Enter)
-            {
-                return;
+                var allText = File.ReadAllText(file);
+                _printings.Add(allText);
             }
 
-            LoadUrl(urlTextBox.Text);
+            var browser = BuildChromiumWebBrowserWithLoadedHtml();
+            this.ShowPrintPreview(browser);
         }
 
-        private void LoadUrl(string url)
+        private void ShowPrintPreview(ChromiumWebBrowser browser)
         {
-            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
-            {
-                browser.Load(url);
-            }
+            var printPreview = new PrintPreview();
+            printPreview.Controls.Add(browser);
+            printPreview.Closed += PrintPreviewOnClosed;
+            printPreview.Show();
         }
 
-        private void ShowDevToolsMenuItemClick(object sender, EventArgs e)
+        private void PrintPreviewOnClosed(object sender, EventArgs eventArgs)
         {
-            browser.ShowDevTools();
+            var browser = this.BuildChromiumWebBrowserWithLoadedHtml();
+            ShowPrintPreview(browser);
+        }
+
+        private ChromiumWebBrowser BuildChromiumWebBrowserWithLoadedHtml()
+        {
+            var printing = this.GetRandomPrinting();
+            var browser = new ChromiumWebBrowser(string.Empty);
+            browser.LoadHtml(printing, @"c:\");
+            return browser;
+        }
+
+        private string GetRandomPrinting()
+        {
+            var index = this.random.Next(0, this._printings.Count);
+            var printing = this._printings[index];
+            return printing;
         }
     }
 }
